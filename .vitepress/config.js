7dbc035851e5f0cf305bc105f92559522bb58cfa
HumbleMaxiu <13394386714@163.com>
2023-04-10 16:41:07 +0800

    feat：项目初始化

---
+++ .vitepress/config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: "My Blog",
+  description: "learn and share",
+  base: '/blog/',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
